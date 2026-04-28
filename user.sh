d590dbecd617047eea7f6ae71f0eb9f5e69f643f
Leo-aab <leonardo45abreu@gmail.com>
2026-04-28 16:18:22 -0300

    Refining script

--- user.sh
+++ user.sh
@@ -1,22 +1,46 @@
 #!/bin/bash
 
 #user create
-# precisando rodar com root
-# lendo o sudo
+# root privilegios - validação 
 if [[ $EUID -ne 0 ]]; then
-    echo "This script must be run as root."
+    echo "Script necessário ser executado como root"
     exit 1
 fi
 
 
-read -p "Enter writing an username: " $USERNAME
-read -p "Enter writing a full username: " $FULL_USERNAME
-read -s -p "Enter writing a good password (recommend 6 carachters): " $PASSWRD
-echo
+read -p "Digite seu usuário  " USERNAME
+if id "$USERNAME" &>/dev/null; then
+   echo "ERRO - O USUÁRIO $USERNAME já existe no sistema"
+   exit 1
+fi
 
-useradd -m -c "$FULL_USERNAME" -s /bin/bash "$USERNAME"
+read -p "Digite seu nome completo:  " FULL_NAME
 
-echo "$USERNAME:$PASSWRD" | chpasswd
+read -s -p "Senha - mínimo 8 caracteres " PASSWORD
+echo ""
+
+#Validação de senha
+if [${#PASSWORD} -lt 8]; then
+   echo "ERRO - SENHA MUITO CURTA - USE PELO MENOS 8 CARACTERES"
+   exit 1
+fi
 
 
-echo "Usuário $USERNAME criado com sucesso."
+useradd -m -c "$FULL_NAME" -s /bin/bash "$USERNAME"
+
+if [ $? -eq 0]; then
+ echo "$USERNAME:$PASSWORD" | chpasswd
+ passwd --expire "$USERNAME"
+ # forçar inicializaçãocom senha resetada
+ # impede acesso de outros integrante da equipe de TI
+
+
+ echo "USUÁRIO $USERNAME CRIADO"
+ echo "NOTA --- SERÁ NECESSÁRIO A ALTERAÇÃO DE SENHA NO PRIMEIRO ACESSO"   
+else
+   echo "ERRO FALHA CRÍTICA AO CRIAR O SISTEMA"
+   exit 1 
+fi
+
+
+
